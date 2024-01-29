2d0060071a25eba9f4549a261d07d9c1ab206ce4
Alina Aufenanger <alina-aufenanger@gmx.de>
2024-01-29 23:14:10 +0100

    updated and added files

---
+++ notebooks/functions.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from datetime import datetime, timezone
+
+from sklearn.preprocessing import PowerTransformer
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, calinski_harabasz_score
+
+import pickle
+
+
+def customer_clusters(df: pd.DataFrame, recency= 'recency', frequency= 'frequency', monetary= 'monetary', k= 3):
+    """
+    Input data frame with RFM-alike columns that will be used for clustering.
+    Function scales variables and performs a KMeans Clustering with k=3 default.
+    Returns df with appended Kmeans Clusters.
+    """
+
+    X = df[[recency, frequency, monetary]]
+
+    scaler = StandardScaler()
+    scaler.fit(X)
+    X_scaled = scaler.transform(X)
+    
+    #filename = "scaler.pickle" # Path with filename
+
+    #with open(filename, "wb") as file:
+    #        pickle.dump(scaler,file)
+
+    X_scaled_df = pd.DataFrame(X_scaled, columns = X.columns)
+
+
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X_scaled_df)
+    X['cluster_kmean'] = kmeans.predict(X_scaled_df)
+
+    return X
+
+
+def customer_info_for_timeframe(df, start= '2023-01-01', end= '2023-06-30'):
+    """
+    Filters given dataframe on given timeframe and returns new customers_with_orders info csv.
+    """
+    
+    # first creating users with orders df:
+    df['order_created_at'] = pd.to_datetime(df['order_created_at'])
+    filtered_df = df[(df['order_created_at'] >= pd.Timestamp(start, tz='UTC')) & (df['order_created_at'] <= pd.Timestamp(end, tz='UTC'))]
+    customer_df = filtered_df.groupby(['user_id', 'registered_on', 'age', 'gender', 'city', 'country']).agg({
+        'order_created_at': ['min', 'max'],  # Min and Max for recency
+        'order_id': 'nunique',                # Number of orders for frequency
+        'num_of_item': 'mean',                # Average number of items of order
+        'revenue': ['sum', 'mean']            # Total revenue of orders (CLV) and average revenugre
+    }).reset_index()
+
+
+    customer_df.columns = ['user_id', 'registration_date', 'age', 'gender', 'city',
+                           'country', 'first_order_date', 'last_order_date', 
+                           'total_orders', 'avg_order_items', 'total_revenue', 'avg_order_value']
+    
+    customer_df['order_frequency_per_year'] = customer_df['total_orders'] / (pd.to_datetime('now', utc=True) - customer_df['first_order_date']).dt.total_seconds() / (60 * 60 * 24 * 30 *12)
+    customer_df['days_since_first_order'] = (pd.to_datetime('now', utc=True) - customer_df['first_order_date']).dt.total_seconds() / (60 * 60 * 24)
+    customer_df['days_since_last_order'] = (pd.to_datetime('now', utc=True) - customer_df['last_order_date']).dt.total_seconds() / (60 * 60 * 24)
+    customer_df['days_since_registration'] = (pd.to_datetime('now', utc=True) - customer_df['registration_date']).dt.total_seconds() / (60 * 60 * 24)
+    customer_df['registration_year'] = customer_df['registration_date'].dt.year
+    customer_df['registration_month'] = customer_df['registration_date'].dt.month
+    customer_df['days_until_first_order'] = (customer_df['first_order_date'] - customer_df['registration_date']).dt.total_seconds() / (60 * 60 * 24)
+
+    # as mentioned before, there are users that haven't ordered yet
+    customer_df['has_ordered'] = customer_df['first_order_date'].apply(lambda x: 0 if pd.isna(x) else 1)
+    
+    customer_df['user_id'] = customer_df['user_id'].astype(object)
+    
+    # Adding age groups:
+    bins = [0, 18, 30, 40, 50, 60, 70, 120]
+    labels = ['0-18', '19-30', '31-40', '41-50', '51-60', '61-70', '71+']
+
+    customer_df['age_class'] = pd.cut(customer_df['age'], bins=bins, labels=labels, right=False)
+    
+        
+    return new_df
+
+def error_metrics(model: str, X_train: str, X_test: str):
+    """Generate error metrics report based on predictions of the model
+    """
+    # Make predictions on the training and test sets
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+    
+    # Evaluate the model
+    mae_train = mean_absolute_error(y_train, y_train_pred)
+    mae_test = mean_absolute_error(y_test, y_test_pred)
+
+    mse_train = mean_squared_error(y_train, y_train_pred)
+    mse_test = mean_squared_error(y_test, y_test_pred)
+
+    rmse_train = mean_squared_error(y_train, y_train_pred, squared=False)
+    rmse_test = mean_squared_error(y_test, y_test_pred, squared=False)
+
+    r2_train = r2_score(y_train, y_train_pred)
+    r2_test = r2_score(y_test, y_test_pred)
+    
+    # Create a DataFrame with the error metrics
+    error_df = pd.DataFrame({
+        'Metric': ['MAE', 'MSE', 'RMSE', 'R2'],
+        'Train': [mae_train, mse_train, rmse_train, r2_train],
+        'Test': [mae_test, mse_test, rmse_test, r2_test]})
+
+    return error_df
